cd56a50e02b96e91c57961f4d52837d35e90f5f2
Steve Baumann <baumann.istvan94@gmail.com>
2021-08-18 12:41:52 +0200

    netlify debug3

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
                     <Nav.Item as="h6" ><li><FontAwesomeIcon icon={faPhoneAlt} />+36(34)319-134</li></Nav.Item>
                     <Nav.Item as="h6"><li><FontAwesomeIcon icon={faMobileAlt} /> +36(70)930-4890</li></Nav.Item>
                     <Nav.Link href="/">Főoldal</Nav.Link>
-                    <Nav.Link href="/about">Rólunk</Nav.Link>
+                    <Nav.Link to="/about" href="/about">Rólunk</Nav.Link>
                     <Nav.Link href="/contact">Kapcsolat</Nav.Link>
                     <Nav.Link href="https://www.facebook.com/resinemat"><FontAwesomeIcon icon={faFacebook} size="2x" /> </Nav.Link>
                   </Nav>

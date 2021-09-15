fff6168f46f93d76669f092abd125660aeed330b
Steve Baumann <baumann.istvan94@gmail.com>
2021-09-15 11:15:33 +0200

    cookie component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-
+import CookieConsent from "react-cookie-consent";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMobileAlt, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
@@ -28,40 +28,52 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App layer">
-            <Navbar bg="dark" variant="dark" expand="lg">
-              <Container>
-                <Navbar.Brand href="/"><img className="" src={logo2} alt="logo2" width="70vh" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ms-auto">
-                    <Nav.Item as="h6" ><li><FontAwesomeIcon icon={faPhoneAlt} />+36(34)319-134</li></Nav.Item>
-                    <Nav.Item as="h6"><li><FontAwesomeIcon icon={faMobileAlt} /> +36(70)930-4890</li></Nav.Item>
-                    <Nav.Link href="/">Főoldal</Nav.Link>
-                    <Nav.Link href="/about">Rólunk</Nav.Link>
-                    <Nav.Link href="/contact">Kapcsolat</Nav.Link>
-                    <Nav.Link href="https://www.facebook.com/resinemat"><FontAwesomeIcon icon={faFacebook} size="2x" /> </Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
-            <Switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about" component={About}></Route>
-              <Route exact path="/contact" component={Contact}></Route>
-            </Switch>
-            <Container className="footer">
-              <Row className="text-center footer-text">
-                <Col xs={12} sm={12} md={6} lg={4} >
-                  <p>Adatvédelmi tájékoztató</p>
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} >
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} >
-                  <p>Resinemat Kft 2800, Tatabánya Tavaszmező u 4/b. </p>
-                </Col>
-              </Row>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand href="/"><img className="" src={logo2} alt="logo2" width="70vh" /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  <Nav.Item as="h6" ><li><FontAwesomeIcon icon={faPhoneAlt} />+36(34)319-134</li></Nav.Item>
+                  <Nav.Item as="h6"><li><FontAwesomeIcon icon={faMobileAlt} /> +36(70)930-4890</li></Nav.Item>
+                  <Nav.Link href="/">Főoldal</Nav.Link>
+                  <Nav.Link href="/about">Rólunk</Nav.Link>
+                  <Nav.Link href="/contact">Kapcsolat</Nav.Link>
+                  <Nav.Link href="https://www.facebook.com/resinemat"><FontAwesomeIcon icon={faFacebook} size="2x" /> </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
             </Container>
+          </Navbar>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/about" component={About}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+          </Switch>
+          <Container className="footer">
+            <Row className="text-center footer-text">
+              <Col xs={12} sm={12} md={6} lg={4} >
+                <p><a href="https://www.w3schools.com">Adatvédelmi tájékoztató</a></p>
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={4} >
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={4} >
+                <p>Resinemat Kft 2800, Tatabánya Tavaszmező u 4/b. </p>
+              </Col>
+            </Row>
+          </Container>
         </div >
+        <CookieConsent
+          location="bottom"
+          declineButtonText="Nem járulok hozzá"
+          buttonText="Hozzájárulok a cookie használathoz"
+          enableDeclineButton
+          cookieName="cookie használat"
+          style={{ background: "#B56636" }}
+          expires={365}
+        ><p className="cookie-text">
+        Ez a weboldal Cookie-kat használ<br/>
+        <span style={{ fontSize: "10px" }}>Cookie-kat használunk a tartalom és a közösségi média funkcióinak biztosításához és a forgalmunk elemzéséhez. <br/>Bővebb információ az <a href="https://www.w3schools.com"><strong>adatvédelmi tájékoztatóról itt</strong></a></span></p>
+      </CookieConsent>
       </Router>);
   }
 }

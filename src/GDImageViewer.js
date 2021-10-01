fd67018d25c57e083a3976e49689c19d380a0d13
Steve Baumann <baumann.istvan94@gmail.com>
2021-10-01 13:15:27 +0200

    gallery test21

--- src/GDImageViewer.js
+++ src/GDImageViewer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import * as R from 'ramda'
 import "./GDImageViewer.css";
 function GDImageViewer(data) {
